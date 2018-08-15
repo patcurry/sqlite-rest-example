29ca98404cae8e07f45a22f6198d43e35f565758
patcurry <currypat1985@gmail.com>
2018-08-15 23:01:06 +0200

    got all the express functions working with the toy data

--- app/scidata.js
+++ app/scidata.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const router = express.Router();
-//const movies = [
-//    {id: 101, name: "Fight Club", year: 1999, rating: 8.1},
-//    {id: 102, name: "Inception", year: 2010, rating: 8.7},
-//    {id: 103, name: "The Dark Knight", year: 2008, rating: 9},
-//    {id: 104, name: "12 Angry Men", year: 1957, rating: 8.9}
-//];
 
 const scidata = [
     //    {id: , title: , shorttitle:, contactperson:, involvedscientists:, department:, keywords:, taxon:, study:},
@@ -58,8 +52,6 @@ router.get('/:id([0-9]{3,})', (req, res) => {
 router.post('/', (req, res) => {
     //Check if all fields are provided and are valid:
     if(!req.body.title ||
-       //!req.body.year.toString().match(/^[0-9]{4}$/g) ||
-       //!req.body.rating.toString().match(/^[0-9]\.[0-9]$/g)){
        !req.body.contactperson ||
        !req.body.involvedscientists ||
        !req.body.department ||
@@ -87,58 +79,73 @@ router.post('/', (req, res) => {
 });
 
 
-/*
 
-  router.put('/:id', function(req, res){
-  //Check if all fields are provided and are valid:
-  if(!req.body.name ||
-  !req.body.year.toString().match(/^[0-9]{4}$/g) ||
-  !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g) ||
-  !req.params.id.toString().match(/^[0-9]{3,}$/g)){
-  
-  res.status(400);
-  res.json({message: "Bad Request"});
-  } else {
-  //Gets us the index of movie with given id.
-  var updateIndex = movies.map(function(movie){
-  return movie.id;
-  }).indexOf(parseInt(req.params.id));
-  
-  if(updateIndex === -1){
-  //Movie not found, create new
-  movies.push({
-  id: req.params.id,
-  name: req.body.name,
-  year: req.body.year,
-  rating: req.body.rating
-  });
-  res.json({message: "New movie created.", location: "/movies/" + req.params.id});
-  } else {
-  //Update existing movie
-  movies[updateIndex] = {
-  id: req.params.id,
-  name: req.body.name,
-  year: req.body.year,
-  rating: req.body.rating
-  };
-  res.json({message: "Movie id " + req.params.id + " updated.", 
-  location: "/movies/" + req.params.id});
-  }
-  }
-  });
 
-  router.delete('/:id', function(req, res){
-  var removeIndex = movies.map(function(movie){
-  return movie.id;
-  }).indexOf(req.params.id); //Gets us the index of movie with given id.
-  
-  if(removeIndex === -1){
-  res.json({message: "Not found"});
-  } else {
-  movies.splice(removeIndex, 1);
-  res.send({message: "Movie id " + req.params.id + " removed."});
-  }
-  });
-*/
+router.put('/:id', function(req, res){
+    //Check if all fields are provided and are valid:
+    ////    if(!req.body.name ||
+    //      !req.body.year.toString().match(/^[0-9]{4}$/g) ||
+    //     !req.body.rating.toString().match(/^[0-9]\.[0-9]$/g) ||
+    //    !req.params.id.toString().match(/^[0-9]{3,}$/g)){
+    
+    if(!req.body.title ||
+       !req.body.contactperson ||
+       !req.body.involvedscientists ||
+       !req.body.department ||
+       !req.body.keywords ||
+       !req.body.study ||
+       !req.params.id.toString().match(/^[0-9]{3,}$/g)
+      ) {
+        
+        res.status(400);
+        res.json({message: "Bad Request"});
+    } else {
+        //Gets us the index of scidata item with given id.
+        var updateIndex = scidata.map(function(dat){
+            return dat.id;
+        }).indexOf(parseInt(req.params.id));
+        
+        if(updateIndex === -1){
+            //Data not found, create new
+            scidata.push({
+                id: req.params.id,
+                title: req.body.title,
+                contactperson: req.body.contactperson,
+                involvedscientists: req.body.involvedscientists,
+                department: req.body.department,
+                keywords: req.body.keywords,
+                study: req.body.study
+
+            });
+            res.json({message: "New data created.", location: "/scidata/" + req.params.id});
+        } else {
+            //Update existing scidata 
+            scidata[updateIndex] = {
+                id: req.params.id,
+                title: req.body.title,
+                contactperson: req.body.contactperson,
+                involvedscientists: req.body.involvedscientists,
+                department: req.body.department,
+                keywords: req.body.keywords,
+                study: req.body.study
+            };
+            res.json({message: "Data id " + req.params.id + " updated.", 
+                      location: "/scidata/" + req.params.id});
+        }
+    }
+});
+
+router.delete('/:id', function(req, res){
+    var removeIndex = scidata.map(function(dat){
+        return dat.id;
+    }).indexOf(req.params.id); //Gets us the index of data with given id.
+    
+    if(removeIndex === -1){
+        res.json({message: "Not found"});
+    } else {
+        scidata.splice(removeIndex, 1);
+        res.send({message: "Data id " + req.params.id + " removed."});
+    }
+});
 
 module.exports = router;
